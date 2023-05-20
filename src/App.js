2b81f0088547f23541b2dd71ba076d9a22e3fd4f
CleverlyCodes <100027510+CleverlyCodes@users.noreply.github.com>
2023-05-20 16:54:24 +0800

    add firebase google authentication (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDX0-KO9kkfYDEod9Y1EAOC9fyQtFbEwIE",
+  authDomain: "homemaker-9fa66.firebaseapp.com",
+  projectId: "homemaker-9fa66",
+  storageBucket: "homemaker-9fa66.appspot.com",
+  messagingSenderId: "985520909790",
+  appId: "1:985520909790:web:249a4f649bcc6afd2e814f",
+  measurementId: "G-H952G52LEB"
+};
+export default config;
