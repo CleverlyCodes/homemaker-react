8664197fcd8ad6f78e4754f796e6c9d84e41fd38
CleverlyCodes <cleverly.codes@gmail.com>
2023-05-21 16:34:49 +0800

    remove useEffect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import firebaseConfig from '../firebaseConfig';
 import { initializeApp } from "firebase/app";
 import {
